e40dbb68f1c9068e60e6783d1f011e5e977ce76a
Diazzzz-cpu <devdiaz420@gmail.com>
2025-11-21 13:10:56 +0800

    Integrate attendance summaries into payroll calculations

--- SchedulePayrollBlazor/Services/IAttendanceService.cs
+++ SchedulePayrollBlazor/Services/IAttendanceService.cs
@@ -10,5 +10,6 @@ public interface IAttendanceService
     Task<TimeLogResult> ClockInAsync(int employeeId);
     Task<TimeLogResult> ClockOutAsync(int employeeId);
     Task<List<DailyAttendanceDto>> GetAttendanceForEmployeeAsync(int employeeId, DateOnly start, DateOnly end);
+    Task<AttendancePeriodSummary> GetSummaryForEmployeeAsync(int employeeId, DateOnly start, DateOnly end);
     Task<PaginatedAttendanceAdminView> GetAttendanceOverviewAsync(DateOnly date, int? employeeIdFilter, int page, int pageSize);
 }
